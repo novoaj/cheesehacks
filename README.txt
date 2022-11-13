0f8d15e8fd660375baaa7df5b2706e055f502f3d
Jared Novoa <jnovoa@wisc.edu>
2022-11-13 08:21:25 -0600

    cleaning up repo

--- README.txt
+++
@@ -1,7 +0,0 @@
-IDEAS: 
-    Game of some sort
-        tic tac toe: keeps track of score for a particular user?
-        mine sweeper
-
-
-    "useful" application: calendar or something 
--- test.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-import os
-# Set environment variable
-os.environ['TK_SILENCE_DEPRECATION'] = "1"
-root = Tk()
-frm = ttk.Frame(root,padding = 10)
-frm.grid()
-root.mainloop()
